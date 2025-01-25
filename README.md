e50845794f3acb97037dee43ba838ed076c1fd3c
nottomcline <tomcline.creator@gmail.com>
2025-01-25 17:44:37 +0100

    update voice output, remove pyttsx3 add openAI model gpt-4o-audio-preview, add simpleaudio to requirements.txt, add install tutorial in readme for simpleaudio

--- README.md
+++ README.md
@@ -5,6 +5,19 @@ This is a project to setup your very Ai Assistant using OpenAi.
 ### Installation
 
 1. Install packages using `pip install -r requirements.txt` in the root directory
+   Note that you will need `Visual C++ Build Tool` for `simpleaudio`. Ensure you have installed the correct components of the Microsoft Build Tools.
+   For Visual Studio 2022, follow these steps to install a required packages for `Visual C++ Build Tool`:
+
+    - Open the Visual Studio Installer.
+    - Click on Modify for your Visual Studio 2022 installation.
+    - Go to the Workloads tab.
+    - Under Desktop Development with C++, ensure you have selected:
+        - MSVC v143 - VS 2022 C++ x64/x86 build tools [today's latest]
+        - C++ CMake tools for Windows
+        - Go to the Individual Components tab and check the following: Windows 10/11 SDK (latest version) [normally the latest in the list]
+        - C++/CLI support
+    - Apply the changes and wait for the installation to complete.
+
 2. Get a OpenAI API Key at [OpenAPIKey](https://openai.com/api/)
 3. Rename `.env.example` to `.env` and enter API Keys:
 
--- Voice_GPT.py
+++ Voice_GPT.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
+import base64
 from collections.abc import Iterable
 
-import pyttsx3
+import simpleaudio as sa
 import speech_recognition as sr
 from openai import OpenAI
 
@@ -11,7 +12,6 @@ from credentials import OPENAI_API_KEY
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 # initialize the text-to-speech engine
-engine = pyttsx3.init()
 LANGUAGE = "de-DE"
 
 
@@ -28,21 +28,22 @@ def transcribe_audio_to_text(filename: str) -> str:
 def generate_response(
     prompt: str | list[str] | Iterable[int] | Iterable[Iterable[int]],
 ) -> str:
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            }
-        ],
-        model="gpt-4o",
+    audio_completion = client.chat.completions.create(
+        model="gpt-4o-audio-preview",
+        modalities=["text", "audio"],
+        audio={"voice": "echo", "format": "wav"},
+        messages=[{"role": "user", "content": prompt}],
+        max_completion_tokens=4000,
+        n=1,
+        temperature=0.5,
     )
-    return chat_completion.choices[0].message.content
+    return audio_completion.choices[0].message.audio.data
 
 
-def speak_text(text: str):
-    engine.say(text)
-    engine.runAndWait()
+def play_wav(file_path: str):
+    wave_obj = sa.WaveObject.from_wave_file(file_path)
+    play_obj = wave_obj.play()
+    return play_obj
 
 
 def main():
@@ -56,7 +57,7 @@ def main():
                 transcription = recognizer.recognize_google(audio, language=LANGUAGE)
                 if transcription.lower() == "hey tom":
                     # Record audio
-                    filename = "own_recorded_audio.wav"
+                    own_recorded_audio_file = "own_recorded_audio.wav"
                     print("Say your question...")
                     with sr.Microphone() as source:
                         recognizer = sr.Recognizer()
@@ -64,23 +65,44 @@ def main():
                         audio = recognizer.listen(
                             source, phrase_time_limit=None, timeout=None
                         )
-                        with open(filename, "wb") as f:
+                        with open(own_recorded_audio_file, "wb") as f:
                             f.write(audio.get_wav_data())
 
                     # Transcribe audio to text
-                    text = transcribe_audio_to_text(filename)
+                    text = transcribe_audio_to_text(own_recorded_audio_file)
                     if text:
                         print(f"you said: {text}")
 
                         # Generate response using GPT
-                        response = generate_response(text)
-                        print(f"GPT says: {response}")
+                        audio_data_response = generate_response(text)
 
-                        # Read response using text-to-speech
-                        speak_text(response)
+                        # Generate the audio response
+                        wav_bytes = base64.b64decode(audio_data_response)
+                        gpt_response_audio_file = "gpt-response_audio.wav"
+                        with open(gpt_response_audio_file, "wb") as f:
+                            f.write(wav_bytes)
+
+                        # Print audio response as text
+                        text_response = transcribe_audio_to_text(
+                            gpt_response_audio_file
+                        )
+                        print(f"GPT says: {text_response}")
+
+                        # Play the WAV file
+                        play_wav(gpt_response_audio_file)
+
             except Exception as e:
                 print(f"An error occurred: {e}")
 
 
+def test():
+    while True:
+        gpt_response_audio_file = "gpt-response_audio.wav"
+        playAudio = play_wav(gpt_response_audio_file)
+
+        if playAudio and playAudio.is_playing():
+            playAudio = None
+
+
 if __name__ == "__main__":
-    main()
+    test()
