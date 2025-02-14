95ff3aa1a08b12cf7ac0be2ff1cb212db57cf232
nottomcline <tomcline.creator@gmail.com>
2025-02-14 20:49:27 +0100

    fixed bug -> wrong location for audio files

--- ai-assistant/prototype-ai-assistant.py
+++ ai-assistant/prototype-ai-assistant.py
@@ -11,9 +11,8 @@ from collections.abc import Iterable
 
 import pyaudio
 import speech_recognition as sr
-from openai import OpenAI
-
 from credentials import OPENAI_API_KEY
+from openai import OpenAI
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
@@ -91,7 +90,7 @@ def main():
                 transcription = recognizer.recognize_google(audio, language=LANGUAGE)
                 if transcription.lower() == "hey tom":
                     # Record audio
-                    own_recorded_audio_file = "/audio/own_recorded_audio.wav"
+                    own_recorded_audio_file = "../audio/own_recorded_audio.wav"
                     print("Say your question...")
                     with sr.Microphone() as source:
                         recognizer = sr.Recognizer()
@@ -112,7 +111,7 @@ def main():
 
                         # Generate the audio response
                         wav_bytes = base64.b64decode(audio_data_response)
-                        gpt_response_audio_file = "/audio/gpt-response_audio.wav"
+                        gpt_response_audio_file = "../audio/gpt-response_audio.wav"
                         with open(gpt_response_audio_file, "wb") as f:
                             f.write(wav_bytes)
 
